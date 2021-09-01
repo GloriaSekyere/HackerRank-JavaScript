ce2fc2be8338d34f7c216c3b82497eb3f7c57527
GloriaSekyere <gloriasekyere15@gmail.com>
2021-09-01 13:52:30 +0000

    Improved solution for reg_ex2

--- day7/2.reg_ex2/reg_ex2.js
+++ day7/2.reg_ex2/reg_ex2.js
@@ -4,10 +4,11 @@ function regexVar() {
      * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
      * followed by one or more letters.
      */
-    const re = /^Mr\.|Mrs\.|Er\.|Dr\.[A-Z]+[a-z]*$/;
+    const re = /^[Dr\.|Mr\.|Mrs\.|Er\.]*[A-Z]+[a-z]*$/;
     
     /*
      * Do not remove the return statement
      */
     return re;
 }
+
